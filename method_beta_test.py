485507b4166d162f853fcf993cde754ffa1b826a
don-c-smith <don.cs.smith@gmail.com>
2025-04-29 13:23:06 -0500

    Set up data upload

--- method_beta_test.py
+++ method_beta_test.py
@@ -1,1 +1,8 @@
+import pandas as pd
+import tadprep as tp
 # This is the file we will use for beta testing of public-facing methods before subsequent debugging
+
+# Load test data
+df = pd.read_csv(r'C:\Users\doncs\Documents\GitHub\TADPREP\data\river_data.csv')
+# Print check
+print(df)
