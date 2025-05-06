a481939bf2ff687a32ff1ed126250d3d042e7f75
don-c-smith <don.cs.smith@gmail.com>
2025-05-06 12:51:03 -0500

    Prepared for next debugging meeting, assigned refactors

--- method_beta_test.py
+++ method_beta_test.py
@@ -24,14 +24,71 @@ Returns: None
 '''
 Questions:
 - Is the name of this method appropriate?
+Rename to 'summarize'
 - Does it do what a reasonable person would expect it to do?
+Yes
 - Are we missing any major capabilities? Is this all the 'info' we need?
+Check for features which are all ints or floats but typed as strings
+Check for any instances which are all-Null in all features
 - Are there extraneous capabilities present in the method?
+No
 - Are all parameters/modes necessary and/or appropriate?
-- What problems or needed changes were identified? 
+Remove verbose mode entirely. It's too simple/low-detail. Just have this be a zero-param method.
+- What problems or needed changes were identified?
+    1. Remove verbose mode entirely, both from driver function and public method
+    2. Implement check for ints or floats typed as strings
+    3. Implement check for all-Null instances
+
+** Refactoring to be done by Don **
 '''
 
 
+'''
+Testing the subset method:
+This method subsets the input DataFrame according to user specification.
+Parameters: verbose (bool, default=True) 
+    - Controls whether detailed process information and methodological guidance is displayed
+
+Returns: The modified DataFrame as subset by the user's specifications
+'''
+# Test non-verbose mode first
+# df_subset = tp.subset(df, verbose=False)
+
+# Test verbose mode
+df_subset = tp.subset(df, verbose=True)
+
+# Print subsetted dataframe
+print(df_subset)
+
+'''
+Questions:
+- Is the name of this method appropriate?
+Yes
+- Does it do what a reasonable person would expect it to do?
+Yes
+- Are we missing any major capabilities? Is this all the subset capacity we usefully need?
+ No
+- Are there extraneous capabilities present in the method?
+No
+- Are all parameters/modes necessary and/or appropriate?
+Yes
+- What problems or needed changes were identified?
+    1. BUG: Loop is stepping from proportion entry back to feature selection in stratified sampling when invalid input
+        is entered for the proportion to subset. Likely a while loop scope problem.
+    2. When subsetting by date, error messages for entering an invalid date need to be more clear/informative
+        (e.g. if you enter 200 for the year, we need something more informative than "nanosecond error")
+    3. Investigate datetime format handling - what's available, what's missing, is there a better/more complete way?
+        Should the current verbose implementation persist in non-verbose mode?
+    4. The "Randomly dropped 25.0% of instances. 45 instances remain."-type message should print in non-verbose mode.
+        Consider expressing as pre-sampling/post-sampling information. Should be consistent for all sampling methods.
+    5. BUG: Bizarre error message printing when attempting to stratify by feature with missing values: INVESTIGATE
+    6. Asking user for explanation in verbose mode is redundant, if verbose, print explanations
+
+** Refactoring to be done by Gabor ** 
+'''
+
+
+# *** PICK UP TEAM DEBUGGING HERE ***
 '''
 Testing the reshape method:
 This method interactively reshapes the input DataFrame according to user specification.
@@ -56,35 +113,7 @@ Questions:
 - What problems or needed changes were identified? 
 '''
 
-
-'''
-Testing the subset method:
-This method subsets the input DataFrame according to user specification.
-Parameters: verbose (bool, default=True) 
-    - Controls whether detailed process information and methodological guidance is displayed
-
-Returns: The modified DataFrame as subset by the user's specifications
-'''
-# Test non-verbose mode first
-# df_subset = tp.subset(df, verbose=False)
-
-# Test verbose mode
-# df_subset = tp.subset(df, verbose=True)
-
-# Print subsetted dataframe
-# print(df_subset)
-
-'''
-Questions:
-- Is the name of this method appropriate?
-- Does it do what a reasonable person would expect it to do?
-- Are we missing any major capabilities? Is this all the subset capacity we usefully need?
-- Are there extraneous capabilities present in the method?
-- Are all parameters/modes necessary and/or appropriate?
-- What problems or needed changes were identified? 
-'''
-
-
+# DON: CREATE A DATASET WITH OUTLIERS!
 '''
 Testing the find_outliers method:
 This method detects outliers in numerical features of a DataFrame using a specified detection method.
